92afe9f25c711416f65a475e4ae860cb8a0f5e63
srgiri <srgiri@griddynamics.com>
2026-08-26 15:58:39 +0530

    Fix form action wrapper in EditInvoiceForm component

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -18,7 +18,9 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const updateInvoiceWithId = async (formData: FormData) => {
+    await updateInvoice(invoice.id, formData);
+  };
 
   return (
     <form action={updateInvoiceWithId}>
