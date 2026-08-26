911d1feaf3b417caac0804cad4db892e974829c2
srgiri <srgiri@griddynamics.com>
2026-08-26 16:31:28 +0530

    Remove dynamic route config from edit invoice page

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -3,8 +3,6 @@ import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
 
-export const dynamic = 'force-dynamic';
-
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
   const id = params.id;
